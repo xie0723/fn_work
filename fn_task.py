5bb40d34d27f9638dabe9459d351d613d7ff5b97
xie_0723 <xie151007>
2016-09-29 11:32:55 +0800

    添加解绑合伙人功能

--- fn_task.py
+++ fn_task.py
@@ -11,13 +11,15 @@ class Zentao(object):
 		self.referer_url = 'http://zentao.fn.com/index.php?m=project&f=task&projectID=967'
 		self.account = 'wangming.xie'
 		self.password = 'Xie151008'
-		self.session.headers  = {
+		self.create_tasks_url = 'http://zentao.fn.com/index.php?m=task&f=create&project=967'
+
+		self.session = requests.Session()
+
+		self.session.headers = {
 			'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 \
 						(KHTML, like Gecko)Chrome/51.0.2704.103 Safari/537.36',
 		}
-		self.session = requests.Session()
-		self.session.verify = False 
-		self.create_tasks_url = 'http://zentao.fn.com/index.php?m=task&f=create&project=967'
+		self.session.verify = False
 
 	def login_zt(self):
 		data = {
@@ -31,26 +33,28 @@ class Zentao(object):
 
 	def create_tasks(self):
 		content_disposition = {
-							'Content-Disposition: form-data; name="after"':'toTaskList',
-							'Content-Disposition: form-data; name="assignedTo[]"':'wangming.xie',
-							'Content-Disposition: form-data; name="deadline"':'2016-09-23',
-							'Content-Disposition: form-data; name="desc"':'<div>&nbsp;客服三期测试&nbsp;</div>',
-							'Content-Disposition: form-data; name="estimate"':40,
-							'Content-Disposition: form-data; name="estStarted"':'2016-09-19',
-							'Content-Disposition: form-data; name="files[]"':' ',
-							'Content-Disposition: form-data; name="files[]"':' ',
-							'Content-Disposition: form-data; name="labels[]"':' ',
-							'Content-Disposition: form-data; name="labels[]"':' ',
-							'Content-Disposition: form-data; name="mailto[]"':' ',
-							'Content-Disposition: form-data; name="module"':0,
-							'Content-Disposition: form-data; name="name"':'客服三期测试',
-							'Content-Disposition: form-data; name="pri"':1,
-							'Content-Disposition: form-data; name="type"':'test',
-							}
+			'Content-Disposition: form-data; name="after"': 'toTaskList',
+			'Content-Disposition: form-data; name="assignedTo[]"': 'wangming.xie',
+			'Content-Disposition: form-data; name="deadline"': '2016-09-23',
+			'Content-Disposition: form-data; name="desc"': '<div>&nbsp;客服三期测试&nbsp;</div>',
+			'Content-Disposition: form-data; name="estimate"': 40,
+			'Content-Disposition: form-data; name="estStarted"': '2016-09-19',
+			'Content-Disposition: form-data; name="files[]"': ' ',
+			'Content-Disposition: form-data; name="files[]"': ' ',
+			'Content-Disposition: form-data; name="labels[]"': ' ',
+			'Content-Disposition: form-data; name="labels[]"': ' ',
+			'Content-Disposition: form-data; name="mailto[]"': ' ',
+			'Content-Disposition: form-data; name="module"': 0,
+			'Content-Disposition: form-data; name="name"': '客服三期测试',
+			'Content-Disposition: form-data; name="pri"': 1,
+			'Content-Disposition: form-data; name="type"': 'test',
+		}
 		self.session.get('http://zentao.fn.com/index.php?m=task&f=create&project=967')
-		self.session.get('http://zentao.fn.com/index.php?m=story&f=ajaxGetProjectStories&t=html&projectID=967&productID=0&moduleID=10707')
-		resp = self.session.post(url=self.create_tasks_url,data=content_disposition)
-		self.session.get('http://zentao.fn.com/index.php?m=project&f=browse&projectID=967&tab=task',allow_redirects=True)
+		self.session.get(
+			'http://zentao.fn.com/index.php?m=story&f=ajaxGetProjectStories&t=html&projectID=967&productID=0&moduleID=10707')
+		resp = self.session.post(url=self.create_tasks_url, data=content_disposition)
+		self.session.get('http://zentao.fn.com/index.php?m=project&f=browse&projectID=967&tab=task',
+		                 allow_redirects=True)
 		self.session.get('http://zentao.fn.com/index.php?m=project&f=task&projectID=967')
 		print (resp.json())
 
@@ -59,5 +63,3 @@ if __name__ == '__main__':
 	lnzt = Zentao()
 	lnzt.login_zt()
 	lnzt.create_tasks()
-
-
---
+++ fn_unbundling_partner.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+__Author__ = "xiewm"
+__Date__ = '2016/9/29 11:31'
+
