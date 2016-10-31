4206b7cc46d606facf4094c05d7e127a775aec79
xie_0723 <xie151007>
2016-10-31 17:27:31 +0800

    add fn_utils  作为平时飞牛工作时的工具集

--- Fn_Utils/get_fn_beta_guid.py
+++ Fn_Utils/get_fn_beta_guid.py
@@ -7,14 +7,15 @@ import requests
 url = 'http://mem-info.beta1.fn:8080/member_api/searchMember'
 
 
-def get_guid(params):
+# 获取beta上面的账号guid
+def get_guid(params, type_=1):
 	query_data = {
 		'name': params,
-		'type': 2  # 1:email查 2 用手机查 3:用飞牛用户名查 5 用guid查
+		'type': type_  # 1:email查 2 用手机查 3:用飞牛用户名查 5 用guid查
 	}
 	resp = requests.post(url, data=query_data, verify=False)
 
 	print resp.json()['data']['MEM_GUID']
 
 
-get_guid('13545379728')
+get_guid('13545379728', 2)
