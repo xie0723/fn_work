7ffdd9d539def30faa2889200420339d812cd852
xie_0723 <xie151007>
2016-10-31 17:22:23 +0800

    update

--- get_fn_cost.py
+++ get_fn_cost.py
@@ -3,18 +3,22 @@ __Author__ = "xiewm"
 __Date__ = '2016/10/27 14:29'
 import requests
 
-domain_name = 'http://pmadmin-api.beta1.fn'
 
-# 获取每个商品成本
-addr = '/erp_show_api/get_show_main_for_erp'
+class getCostInfo(object):
+	def __init__(self):
+		self.domain_name = 'http://pmadmin-api.beta1.fn'
+		# 获取每个商品成本
+		self.addr = '/erp_show_api/get_show_main_for_erp'
+		self.session = requests.Session()
+		self.session.verify = False
 
-session = requests.Session()
+	def get_cost(self):
+		query_dates = {
+			'data': '{"sm_seq":"201608CM260000173"}',
+			'STK_COUNTRY': 'cn',
+			'STK_COUNTRY_REGION': 'sh'
+		}
 
-query_dates = {
-	'data': '{"sm_seq":"201511CM120000011"}',
-	'STK_COUNTRY': 'cn',
-	'STK_COUNTRY_REGION': 'sh'
-}
-
-resp = session.post(url=(domain_name + addr), data=query_dates, verify=False)
-print float((resp.json()['Body']['data'][0]['IT_SHOW_COST']))
+		resp = self.session.post(url=(self.domain_name + self.addr), data=query_dates)
+		print type(resp.json()['Body']['data'])
+		print float((resp.json()['Body']['data'][0]['IT_SHOW_COST']))
