605c90b2df1cf36e7502d58ca134081e2eacceae
xie_0723 <xie151007>
2016-11-04 14:39:58 +0800

    update

--- Fn_Utils/get_fn_beta_guid.py
+++ Fn_Utils/get_fn_beta_guid.py
@@ -4,18 +4,15 @@ __Date__ = '2016/10/31 17:12'
 
 import requests
 
+# 获取beta 账号的guid
+
 url_beta = 'http://mem-info.beta1.fn:8080/member_api/searchMember'
 url_preview = 'http://mem-info.idc1.fn:8080/member_api/searchMember'
 
+query_data = {
+	'name': '13545379728',
+	'type': 2  # 1:email查 2 用手机查 3:用飞牛用户名查 5 用guid查
+}
 
-# 获取beta上面的账号guid
-def get_guid(params, type_=1):
-	query_data = {
-		'name': params,
-		'type': type_  # 1:email查 2 用手机查 3:用飞牛用户名查 5 用guid查
-	}
-	resp = requests.post(url_preview, data=query_data, verify=False)
-
-	print resp.json()['data']['MEM_GUID']
-
-# get_guid('13545379728', 2)
+resp = requests.post(url_beta, data=query_data, verify=False)
+print resp.json()['data']['MEM_GUID']
