0a9527022176ce758d33b22a65278941574b52bb
xie_0723 <xie151007>
2016-11-15 17:27:02 +0800

    update

--- Fn_Utils/get_fn_beta_guid.py
+++ Fn_Utils/get_fn_beta_guid.py
@@ -8,11 +8,20 @@ import requests
 
 url_beta = 'http://mem-info.beta1.fn:8080/member_api/searchMember'
 url_preview = 'http://mem-info.idc1.fn:8080/member_api/searchMember'
+proxies = {
+	'http': 'http://proxy2.fn.com:8080',
+	'https': 'https://proxy2.fn.com:8080'
+
+}
 
 query_data = {
-	'name': '13545379728',
+	'name': '15575907876',
 	'type': 2  # 1:email查 2 用手机查 3:用飞牛用户名查 5 用guid查
 }
 
-resp = requests.post(url_beta, data=query_data, verify=False)
-print resp.json()['data']['MEM_GUID']
+# beta
+# resp = requests.post(url_beta, data=query_data,verify=False)
+# ------------------------------------------------------------------------------------
+# preview
+resp = requests.post(url_preview, data=query_data, proxies=proxies, verify=False)
+print (resp.json()['data']['MEM_GUID'])
